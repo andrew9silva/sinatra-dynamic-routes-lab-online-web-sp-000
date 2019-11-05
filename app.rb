e17081598d683bb18d806277335963c52a867420
Andrew Silva <andrewsilva9@gmail.com>
2019-11-05 02:08:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ class App < Sinatra::Base
     @word_3 = params[:word3]
     @word_4 = params[:word4]
     @word_5 = params[:word5]
-    "#{@word_1 @word_2 @word_3 @word_4 @word_5}."
+    "#{@word_1} #{@word_2} #{@word_3} #{@word_4} #{@word_5}."
   end
     
 
